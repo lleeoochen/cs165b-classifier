5421655a2cf9a284ee6bd73f2b589634303def5e
Wei Tung Chen <weitung_chen@umail.ucsb.edu>
2018-06-02 23:02:36 -0700

    upgrade

--- prediction.py
+++ prediction.py
@@ -28,7 +28,7 @@ def loadTrainingData():
     print ('Loading training data...')
     X = []
     Y = []
-    test_rate = 0.2
+    test_rate = 0.1
     classes = 10
     class_data = 1000
 
@@ -60,9 +60,10 @@ def loadTestingData():
 
 # Data loading and preprocessing
 print ("Ready to start.")
-import tflearn.datasets.mnist as mnist
 X, Y, validX, validY = loadTrainingData()
 testX = loadTestingData()
+testX = X
+
 X = X.reshape([-1, 28, 28, 1])
 validX = validX.reshape([-1, 28, 28, 1])
 testX = testX.reshape([-1, 28, 28, 1])
@@ -71,23 +72,37 @@ testX = testX.reshape([-1, 28, 28, 1])
 # Building convolutional network
 print ("Ready to build layers.")
 network = input_data(shape=[None, 28, 28, 1], name='input')
+
 network = conv_2d(network, 32, 5, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
 network = local_response_normalization(network)
+
 network = conv_2d(network, 64, 5, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
 network = local_response_normalization(network)
-network = fully_connected(network, 128, activation='tanh')
-network = dropout(network, 0.8)
-network = fully_connected(network, 256, activation='tanh')
-network = dropout(network, 0.8)
+
+network = conv_2d(network, 128, 5, activation='relu', regularizer="L2") #
+network = max_pool_2d(network, 2) #
+network = local_response_normalization(network) #
+
+network = dropout(network, 0.8) #
+network = fully_connected(network, 256, activation='relu') #
+# network = fully_connected(network, 256, activation='tanh')
+# network = dropout(network, 0.8)
+
+network = fully_connected(network, 512, activation='relu') #
+# network = fully_connected(network, 512, activation='tanh')
+# network = dropout(network, 0.8)
+
 network = fully_connected(network, 10, activation='softmax')
 network = regression(network, optimizer='adam', learning_rate=0.01, loss='categorical_crossentropy', name='target')
 
 # Training
 print ("Ready to get fit.")
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit({'input': X}, {'target': Y}, n_epoch=20,
+model.fit({'input': X}, {'target': Y},
+          n_epoch=20,
+          batch_size=50,
           validation_set=({'input': validX}, {'target': validY}),
           snapshot_step=100,
           show_metric=True,
@@ -101,5 +116,6 @@ fstream = open(result_file, 'w+')
 for i in range(0, len(predictedY)):
     fstream.write(str(predictedY[i]) + '\n')
 fstream.close()
+
 # correctRatio = np.mean(np.equal(np.argmax(predictedY, axis=1), np.argmax(Y, axis=1)).astype(int))
 # print("Validation accuracy using predict (max): {}".format(correctRatio))
