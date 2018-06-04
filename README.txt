93dae497acae6ef003ab00b222b475e491c1480c
Wei Tung Chen <lleeoochen@gmail.com>
2018-06-03 23:37:27 -0700

    Took out random states. Added academic integrity file.

---
+++ README.txt
@@ -0,0 +1,5 @@
+CS165B Assignment 4
+Name: Wei Tung Chen
+Perm: 8936502
+
+Reference Source: https://github.com/tflearn/tflearn/blob/master/examples/images/
--- prediction.py
+++ prediction.py
@@ -1,5 +1,43 @@
+# Starter code for CS 165B HW4
 
-# Reference Source: https://github.com/tflearn/tflearn/blob/master/examples/images/
+"""
+Implement the testing procedure here. 
+
+Inputs:
+    Given the folder named "hw4_test" that is put in the same directory of your "predictio.py" file, like:
+    - Main folder
+        - "prediction.py"
+        - folder named "hw4_test" (the exactly same as the uncompressed hw4_test folder in Piazza)
+    Your "prediction.py" need to give the following required output.
+
+Outputs:
+    A file named "prediction.txt":
+        * The prediction file must have 10000 lines because the testing dataset has 10000 testing images.
+        * Each line is an integer prediction label (0 - 9) for the corresponding testing image.
+        * The prediction results must follow the same order of the names of testing images (0.png - 9999.png).
+    Notes: 
+        1. The teaching staff will run your "prediction.py" to obtain your "prediction.txt" after the competition ends.
+        2. The output "prediction.txt" must be the same as the final version you submitted to the CodaLab, 
+        elsewise you will be given 0 score for your hw4.
+
+
+**!!!!!!!!!!Important Notes!!!!!!!!!!**
+    To open the folder "hw4_test" or load other related files, 
+    please use open('./necessary.file') instaed of open('some/randomly/local/directory/necessary.file').
+
+    For instance, in the student Jupyter's local computer, he stores the source code like:
+    - /Jupyter/Desktop/cs165B/hw4/prediction.py
+    - /Jupyter/Desktop/cs165B/hw4/hw4_test
+    If he use os.chdir('/Jupyter/Desktop/cs165B/hw4/hw4_test'), this will cause an IO error 
+    when the teaching staff run his code under other system environments.
+    Instead, he should use os.chdir('./hw4_test').
+
+
+    If you use your local directory, your code will report an IO error when the teaching staff run your code,
+    which will cause 0 socre for your hw4.
+"""
+
+
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
@@ -49,7 +87,7 @@ def loadTrainingData():
             Y.append(label)
             printStatus("\r" + str(i) + ': (' + str(j) + '/1000)')
     print
-    X, validX, Y, validY = train_test_split(X, Y, test_size=0.1, random_state=42)
+    X, validX, Y, validY = train_test_split(X, Y, test_size=test_rate)
     return (np.array(X), np.array(Y), np.array(validX), np.array(validY))
 
 
@@ -110,7 +148,6 @@ print ("Ready to get fit.")
 model = tflearn.DNN(network, tensorboard_verbose=0)
 model.fit({'input': X}, {'target': Y},
           n_epoch=500,
-          shuffle=True,
           batch_size=100,
           validation_set=({'input': validX}, {'target': validY}),
           snapshot_step=100,
